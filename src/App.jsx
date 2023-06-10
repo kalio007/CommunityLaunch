66a6ec7e5edff35340cbfffdfbb9da7949074dec
Kalio007 <Kalioprince1@gmail.com>
2023-06-10 02:43:13 +0100

    fixed the font

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 // import  {hero}  from './Assests';
 import  {hero, ill}  from './Assests';
 
@@ -8,7 +7,7 @@ function App() {
 
   return (
     <body>
-      <div className='flex flex-col space-y-6 h-full w-[320px] md:w-[420px] z-6 items-center mx-auto mt-40'>
+      <div className='flex flex-col envy space-y-6 h-full w-[320px] md:w-[420px] z-6 items-center mx-auto mt-40'>
       <div className='flex flex-col md:flex-row w-full md:w-[400px] justify-between text-[#DFF314]'>
         <div className='underline underline-offset-4 italic '><h3>commnity round detail</h3></div>
         <div className='underline underline-offset-4 italic'><h3>unison docs</h3></div>
@@ -16,7 +15,7 @@ function App() {
       <div className='flex w-full justify-between bg-[#202020] border-[1px] border-white border-solid p-6'>
       <div className='text-xl md:text-3xl'>
         <h1><span className='text-[#DFF314]'>UNISON</span></h1>
-        <h1 className='text-[#FFFF] italic'>
+        <h1 className='text-[#FFFF] italic envy'>
         COMMUNITY 
         <br/>
         ROUND</h1>
@@ -34,20 +33,20 @@ function App() {
         className='bg-[#FFFFFF] w-[90%] p-2 text-l md:text-xl text-center'
         />
 
-        <button className='bg-[#FFFFFF] text-l md:text-2xl border-solid border-l-[1px] border-l-[black] p-1 md:p-2 '>
+        <button className='bg-[#FFFFFF] font-envy text-l md:text-2xl text-[#696969] border-solid border-l-[1px] border-l-[black] p-1 md:p-2 '>
           ETH
         </button>
       </div>
-      <div className='w-full h-[38px] md:h-[47px] bg-[#050FFF] opacity-80 p-2 text-white text-center border-[#FFFFFF] border-[1px] text-[2xl]'>
-        <button >
+      <div className='w-full h-[38px] md:h-[47px] bg-[#050FFF] p-2 text-white text-center border-[#FFFFFF] border-[1px] text-[2xl]'>
+        <button className='bg-[#050FFF] opacity-100' >
           DEPOSIT
         </button>
       </div>
     </div>
     <div className='hidden md:block illustration'>
-    <img src={ill} />
+    <img src={ill} alt='background'/>
     </div>
-    <div className='md:hidden illustration-two'>
+    <div className='md:hidden illustration-two '>
     <img src={ill} />
     </div>
     </body>
